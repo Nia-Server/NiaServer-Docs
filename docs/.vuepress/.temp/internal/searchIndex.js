37ae579b39d33d9390b99b957ea8252bcaedd094
NIANIANKNIA <nianianknia@163.com>
2022-09-03 23:37:54 +0800

    新增搜索框、首页logo

---
+++ docs/.vuepress/.temp/internal/searchIndex.js
@@ -0,0 +1,188 @@
+export const searchIndex = [
+  {
+    "title": "开发者名单",
+    "headers": [],
+    "path": "/developers.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "游玩指南",
+    "headers": [
+      {
+        "level": 3,
+        "title": "安装游戏",
+        "slug": "安装游戏",
+        "link": "#安装游戏",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "进入游戏",
+        "slug": "进入游戏",
+        "link": "#进入游戏",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "申请白名单",
+        "slug": "申请白名单",
+        "link": "#申请白名单",
+        "children": []
+      }
+    ],
+    "path": "/guide.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "服务器历史",
+    "headers": [],
+    "path": "/history.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "服务器图鉴",
+    "headers": [],
+    "path": "/Illustrated.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "",
+    "headers": [],
+    "path": "/",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "规章制度",
+    "headers": [
+      {
+        "level": 3,
+        "title": "序章 注意事项",
+        "slug": "序章-注意事项",
+        "link": "#序章-注意事项",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第一章 服务器总则",
+        "slug": "第一章-服务器总则",
+        "link": "#第一章-服务器总则",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第二章 玩家间相关规定",
+        "slug": "第二章-玩家间相关规定",
+        "link": "#第二章-玩家间相关规定",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第三章 游戏内相关规定",
+        "slug": "第三章-游戏内相关规定",
+        "link": "#第三章-游戏内相关规定",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第四章 群聊相关规定",
+        "slug": "第四章-群聊相关规定",
+        "link": "#第四章-群聊相关规定",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第五章 公会相关规定",
+        "slug": "第五章-公会相关规定",
+        "link": "#第五章-公会相关规定",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第六章 圈地系统相关规定",
+        "slug": "第六章-圈地系统相关规定",
+        "link": "#第六章-圈地系统相关规定",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "第七章 管理人员相关规定",
+        "slug": "第七章-管理人员相关规定",
+        "link": "#第七章-管理人员相关规定",
+        "children": []
+      }
+    ],
+    "path": "/regulation.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "特别鸣谢",
+    "headers": [],
+    "path": "/specialThanks.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "服务器更新日志",
+    "headers": [
+      {
+        "level": 3,
+        "title": "v4版本暂时没有开放",
+        "slug": "v4版本暂时没有开放",
+        "link": "#v4版本暂时没有开放",
+        "children": []
+      }
+    ],
+    "path": "/update.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "",
+    "headers": [],
+    "path": "/en-US/",
+    "pathLocale": "/en-US/",
+    "extraFields": []
+  },
+  {
+    "title": "Ntrade使用方法",
+    "headers": [
+      {
+        "level": 3,
+        "title": "初始化插件",
+        "slug": "初始化插件",
+        "link": "#初始化插件",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "修改配置文件",
+        "slug": "修改配置文件",
+        "link": "#修改配置文件",
+        "children": []
+      },
+      {
+        "level": 3,
+        "title": "开始使用",
+        "slug": "开始使用",
+        "link": "#开始使用",
+        "children": []
+      }
+    ],
+    "path": "/plugins/Ntrade.html",
+    "pathLocale": "/",
+    "extraFields": []
+  },
+  {
+    "title": "",
+    "headers": [],
+    "path": "/404.html",
+    "pathLocale": "/",
+    "extraFields": []
+  }
+]
