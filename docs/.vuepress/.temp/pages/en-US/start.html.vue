a14fd9167ed2c72a226ad4631104c26f7b3b799f
NIANIANKNIA <nianianknia@163.com>
2023-03-06 17:46:13 +0800

    新增自动部署

---
+++ docs/.vuepress/.temp/pages/en-US/start.html.vue
@@ -0,0 +1,16 @@
+<template><div><h1 id="🎮before-playing" tabindex="-1"><a class="header-anchor" href="#🎮before-playing" aria-hidden="true">#</a> 🎮BEFORE PLAYING</h1>
+<div class="custom-container warning"><p class="custom-container-title">WARNING</p>
+<p>Pages are editing The page is in editing. It may be imperfect, and affect your reading！</p>
+</div>
+<p>Before playing NIA Server, join our confirmed group, <strong>or you can't play!</strong></p>
+<p><a href="https://jq.qq.com/?_wv=1027&amp;k=qsGBYtfP" target="_blank" rel="noopener noreferrer">Press me to join the group<ExternalLinkIcon/></a></p>
+<p>No can do? <a href="https://qm.qq.com/cgi-bin/qm/qr?k=7mnlY_HJbyNa8zZ94amitHNMmayXDyHS&amp;noverify=0&amp;personal_qrcode_source=4" target="_blank" rel="noopener noreferrer">Press here to question the server master on QQ<ExternalLinkIcon/></a></p>
+<div class="custom-container tip"><p class="custom-container-title">Here are NIA Server official QQ groups, be careful to tell the fake.  NIA Main Group：724360499</p>
+<p>NIA Confirm Group：371417932</p>
+<p>NIA Backup Group：258515673</p>
+<p>NIA OP REG Group：543363488</p>
+<p>NIA Master QQ：1020317403</p>
+</div>
+</div></template>
+
+
---
+++ docs/.vuepress/.temp/pages/zh-CN/history.html.vue
@@ -0,0 +1,24 @@
+<template><div><h1 id="📒服务器历史-编写中" tabindex="-1"><a class="header-anchor" href="#📒服务器历史-编写中" aria-hidden="true">#</a> 📒服务器历史（编写中...）</h1>
+<div class="custom-container warning"><p class="custom-container-title">编写中页面提醒</p>
+<p>本页面仍处于编写状态中，内容可能不全面，会对阅读造成一定的影响！</p>
+</div>
+<h3 id="群聊黑历史截图" tabindex="-1"><a class="header-anchor" href="#群聊黑历史截图" aria-hidden="true">#</a> 群聊黑历史截图</h3>
+<p><strong>收录自NIA服务器群聊群友水群期间，欢迎投稿！</strong></p>
+<p><img src="/history/1.png" alt="1"></p>
+<p><img src="/history/2.png" alt="2"></p>
+<p><img src="/history/3.png" alt="3"></p>
+<p><img src="/history/4.png" alt="4"></p>
+<p><img src="/history/5.jpg" alt="5"></p>
+<p><img src="/history/6.png" alt="6"></p>
+<p><img src="/history/7.png" alt="7"></p>
+<p><img src="/history/8.png" alt="8"></p>
+<p><img src="/history/9.jpg" alt="9"></p>
+<p><img src="/history/10.jpg" alt="10"></p>
+<p><img src="/history/11.jpg" alt="11"></p>
+<p><img src="/history/12.jpg" alt="12"></p>
+<p><img src="/history/13.jpg" alt="13"></p>
+<p><img src="/history/14.png" alt="14"></p>
+<p>期待您的投稿！</p>
+</div></template>
+
+
